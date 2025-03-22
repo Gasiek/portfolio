4b617e2ff65f5b4622bfc20ccfdb77f487b966a4
Gasiek <kacper.b.gasior@gmail.com>
2025-03-22 22:43:31 +0100

    Updated site configuration to work on github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '@Utils/styles/GlobalStyles';
 import theme from '@Utils/styles/theme';
@@ -10,14 +10,14 @@ import Footer from '@Templates/Footer';
 const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <BrowserRouter>
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects/:id" element={<Project />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </Router>
   </ThemeProvider>
 );
 
